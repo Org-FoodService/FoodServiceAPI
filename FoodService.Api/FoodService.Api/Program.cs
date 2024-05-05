cfc4166d4b72d41a00627ad0b9a61976077c545c
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2024-05-05 13:50:25 -0300

    FIX: System.IO.FileNotFoundException when build in actions github

--- FoodService.Api/FoodService.Api/Program.cs
+++ FoodService.Api/FoodService.Api/Program.cs
@@ -9,13 +9,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
